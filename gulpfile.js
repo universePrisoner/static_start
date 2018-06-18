619233b26b9ca8d7be9652034466f05409b93d48
Anatoly Volgin <anatoly.volgin.rg@gmail.com>
2018-06-18 16:52:20 +0300

    '1.0.2'

--- gulpfile.js
+++ gulpfile.js
@@ -10,43 +10,32 @@ let gulp = require('gulp'),
 	minIMG = require('gulp-imagemin');
 
 // ==============  SASS  ============== 
-// Use packages: gulp-sass, gulp-postcss, postcss-sorting, gulp-autoprefixer, browser-sync
-gulp.task('sass',function(){
-	return gulp.src('src/superstructure/sass/**/*.sass')
+gulp.task('sass',function(){	
+	return gulp.src('src/blocks/sass/**/*.sass')
 			.pipe(sassGlobe())
-			.pipe(sass())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
 			.pipe(postcss(
 				[sorting({
 					"properties-order": "alphabetical"
 				})]
 			))
-			.pipe(autoprefixer())
-			.pipe(gulp.dest('src/css'))
+			.pipe(gulp.dest('src/static/css'))
 			.pipe(browserSync.stream());
 });
 
 
 
 // ==============  PUG  ============== 
-// Use packages: gulp-pug, del, 
 	gulp.task('pug',function(){
-		return gulp.src('src/superstructure/*.pug')
+		return gulp.src('src/blocks/pug/**/*.pug')
 				.pipe(pug({
 					pretty: true
 				}))
-				.pipe(gulp.dest('src/'));
-	});
-	// Delete Clone Pug Template On the Fly
-	gulp.task('delClone', function(){
-		del('src/_newPage.html');
-	});
-	// Clear custom Pug files
-	gulp.task('clearPugDir', function(){
-		del(['src/superstructure/*.pug','src/superstructure/pug/blocks/**/*.pug']);
+				.pipe(gulp.dest('src/static/'));
 	});
 
 // ==============  DELETE  ============== 
-// Use packages: del
 	// -------------- clear build directory --------------
 	gulp.task('del',function(){
 		del('build/*', {dryRun: true}).then(paths => {
@@ -55,38 +44,36 @@ gulp.task('sass',function(){
 	});
 
 // ==============  LIVE RELOAD (BROWSER SYNC)  ============== 
-// Use packages: gulp-sass, gulp-postcss, postcss-sorting, gulp-autoprefixer, browser-sync, del, gulp-pug
 	gulp.task('fly',function(){
 		browserSync.init({
 					server: {
-						baseDir: 'src'
+						baseDir: 'src/static'
 					}}
 			);
-		gulp.watch('src/superstructure/**/*.pug',['pug', 'delClone']);
-		gulp.watch("src/superstructure/sass/**/*.sass", ['sass']);
-		gulp.watch('src/*.html').on('change', browserSync.reload);
-		gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
-		gulp.watch('src/img/**/*').on('change', browserSync.reload);
-		gulp.watch('src/fonts/**/*').on('change', browserSync.reload);
+		gulp.watch('src/blocks/**/*.pug',['pug']);
+		gulp.watch("src/blocks/sass/**/*.sass", ['sass']);
+		gulp.watch('src/static/*.html').on('change', browserSync.reload);
+		gulp.watch('src/static/js/**/*.js').on('change', browserSync.reload);
+		gulp.watch('src/static/img/**/*').on('change', browserSync.reload);
+		gulp.watch('src/static/fonts/**/*').on('change', browserSync.reload);
 
 	});
 
 
 // ==============  BUILD PROJECT  ============== 
-// Use packages: gulp-imagemin
 	gulp.task('build' ,function(){
-		gulp.src('src/*.html').pipe(gulp.dest('build/'));
-		gulp.src('src/css/**/*.css').pipe(gulp.dest('build/css/'));
-		gulp.src('src/js/**/*.js').pipe(gulp.dest('build/js/'));
-		gulp.src('src/images/**/*')
+		gulp.src('src/static/*.html').pipe(gulp.dest('build/'));
+		gulp.src('src/static/css/**/*.css').pipe(gulp.dest('build/css/'));
+		gulp.src('src/static/js/**/*.js').pipe(gulp.dest('build/js/'));
+		gulp.src('src/static/images/**/*')
 			.pipe(minIMG({
 				interlaced: true,
 				progressive: true,
 				optimizationLevel: 5,
 				svgoPlugins: [{removeViewBox: true}]}))
 			.pipe(gulp.dest('build/images/'));
-		gulp.src('src/fonts/**/*').pipe(gulp.dest('build/fonts/'));
-		gulp.src('src/vendor/**/*').pipe(gulp.dest('build/vendor/'));
+		gulp.src('src/static/fonts/**/*').pipe(gulp.dest('build/fonts/'));
+		gulp.src('src/static/vendor/**/*').pipe(gulp.dest('build/vendor/'));
 	});
 
  
