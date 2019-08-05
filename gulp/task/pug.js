bac58f242caf9ed178000339b3c5fa35f80b174e
zeroWheel <anatoly.volgin.rg@gmail.com>
2019-08-05 23:55:30 +0300

    V2: fix gulp-pug

--- gulp/task/pug.js
+++ gulp/task/pug.js
@@ -1,21 +1,39 @@
-module.exports = function () {
-	$.gulp.task('pug',function () {
-		return $.gulp.src($.path.dev.pug + 'pages/*.pug')
-		.pipe($.pug({
-			locals: {
-				// Hack to throw config file to pug
-				index: JSON.parse($.fs.readFileSync('./src/pug/data/dataIndex.json', 'utf-8'))
-			},
-			pretty:true,
-			verbose: true
-		}))
-		.on('error',$.notify.onError(function (error) {
-			return {
-				title: 'Pug',
-				message: error.message
-			}
-		}))
-		.pipe($.gulp.dest($.path.dev.base))
-		.on('end',$.browserSync.reload);
+import * as gulp         from 'gulp';
+import pug               from 'gulp-pug';
+import { onError }       from 'gulp-notify';
+import * as browserSync  from 'browser-sync';
+import fs from 'fs';
+
+
+const compileData = {
+	index : JSON.parse(fs.readFileSync('./src/pug/data/dataIndex.json', 'utf-8'))
+};
+
+
+const compileOptions = {
+	pretty    : true,
+	verbose   : true,
+	locals    : compileData,
+};
+
+
+const compilePug = () => {
+	gulp.task('pug', () => {
+		return gulp.src(`${$.path.dev.pug}pages/*.pug`)
+			.pipe(pug(compileOptions))
+			.on('error', onError( err => {
+				console.log({
+					error    : err.Error,
+					code     : err.code,
+					message  : err.msg,
+					fileName : err.filename
+				});
+				
+				return { title: 'PUG', message: err.code  };
+			}))
+			.pipe( gulp.dest($.path.dev.base) )
+			.on('end', browserSync.reload );
 	});
 }
+
+module.exports = compilePug;
